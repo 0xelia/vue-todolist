aa81234316993fed53d5bff40712acba76611b36
0xelia <elia.vanon@yahoo.com>
2022-08-25 23:15:27 +0200

    Milestone 3

--- js/main.js
+++ js/main.js
@@ -25,12 +25,25 @@ const app = new Vue({
     el: '#root',
     data: {
         tasks: tasks,
-        className: 'todo'
+        className: 'todo',
+        newTask: '',
     },
     methods: {
         checked(task){
             task.done = !task.done
             console.log(task.done)
+        },
+        addTask(){
+            if(this.newTask.trim() !== ''){
+                tasks.push({
+                    text: this.newTask,
+                    done: false
+                })
+                this.newTask = ''
+            }
+        },
+        test(){
+            console.log('press')
         }
     }
 })
