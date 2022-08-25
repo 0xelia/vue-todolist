9e2e8bfb165169a85209aaee4a759eae0af5c9c9
0xelia <elia.vanon@yahoo.com>
2022-08-25 22:44:25 +0200

    Milestone 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-console.log('mandi')
+const tasks = [
+    {
+        text:'Go to the market',
+        done: false
+    },
+    {
+        text: 'Feed the Pyton',
+        done: true
+    },
+    {
+        text:'Fight the dragon',
+        done: false
+    },
+    {
+        text:'Find the ring',
+        done: true
+    },
+    {
+        text:'Walk the dog',
+        done: false
+    }
+]
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        tasks: tasks,
+        className: 'todo'
+    },
+    methods: {
+        checked(task){
+            task.done = !task.done
+            console.log(task.done)
+        }
+    }
+})
+
+console.log(app.tasks)
