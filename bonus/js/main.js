0f16eb72cb39d6939930ad7b2e4cb4aff08c2ca4
0xelia <elia.vanon@yahoo.com>
2022-08-26 10:12:03 +0200

    bonus done

---
+++ bonus/js/main.js
@@ -0,0 +1,59 @@
+const tasks = [
+    {
+        text:'Go to the market',
+        done: false
+    },
+    {
+        text: 'Feed the Pyton',
+        done: true
+    },
+    {
+        text:'Fight the dragon',
+        done: false
+    },
+    {
+        text:'Find the ring',
+        done: true
+    },
+    {
+        text:'Walk the dog',
+        done: false
+    }
+]
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        tasks: tasks,
+        className: 'todo',
+        newTask: '',
+    },
+    computed: {
+        completedTasks: function() {
+            return this.tasks.filter(({ done }) =>  done )
+        },
+        uncompletdTasks: function(){
+            return this.tasks.filter(({done}) => !done)
+        }
+    },
+    methods: {
+        checked(task){
+            task.done = !task.done
+            console.log(task.done)
+        },
+        addTask(){
+            if(this.newTask.trim() !== ''){
+                tasks.push({
+                    text: this.newTask,
+                    done: false
+                })
+                this.newTask = ''
+            }
+        },
+        test(){
+            console.log('press')
+        }
+    }
+})
+
+console.log(app.tasks)
